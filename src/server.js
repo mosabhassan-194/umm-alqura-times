dde34bb06a168f4aa24b1f50a06f6fa9a3b54e67
mosabhassan-194 <mhassn194@gmail.com>
2025-04-12 22:57:33 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,34 +1,23 @@
-const { exec } = require("child_process");
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Athan Clock Server is running");
-});
-
 app.get("/sync", (req, res) => {
-  exec(`
-    cd src && \
+  exec(
+    `
     python3 -m venv venv && \
     . venv/bin/activate && \
     pip install --upgrade pip && \
-    pip install -r ../requirements.txt && \
+    pip install -r requirements.txt && \
     python3 sync.py
-  `,
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Sync failed: ${error.message}`);
-      res.send(`Sync failed: ${error.message}`);
-      return;
+    `,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Sync failed: ${error.message}`);
+        res.send(`Sync failed: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+      }
+      console.log(`stdout: ${stdout}`);
+      res.send("Sync complete.");
     }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-    }
-    console.log(`stdout: ${stdout}`);
-    res.send("Sync complete.");
-  });
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running");
+  );
 });
