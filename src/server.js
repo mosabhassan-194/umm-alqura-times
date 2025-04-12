c85d976471d303cbc66d1e0025d85bfe434df1d8
mosabhassan-194 <mhassn194@gmail.com>
2025-04-12 22:01:53 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Umm Al-Qura API is live!");
+});
+
+// /jazantimes route
+app.get("/jazantimes", (req, res) => {
+  fs.readFile("jazan.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Failed to read jazan.json", err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    res.header("Content-Type", "application/json");
+    res.send(data);
+  });
+});
+
+// Start server
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running...");
+});
